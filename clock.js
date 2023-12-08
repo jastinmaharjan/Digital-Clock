c5550693be5b16c3a79cc08aa57e2823c54b82a1
Justin <mhrjastin@gmail.com>
2023-12-08 12:47:29 +0545

    digital clock

--- clock.js
+++ clock.js
@@ -1,12 +1,16 @@
-let hrs = document.getElementById("hrs");
-let min = document.getElementById("min");
-let sec = document.getElementById("sec");
+document.addEventListener("DOMContentLoaded", function () {
 
-setInterval(() => {
+    let hrs = document.getElementById("hrs");
+    let min = document.getElementById("min");
+    let sec = document.getElementById("sec");
 
-    let currentTime = new Date();
+    setInterval(() => {
 
-    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHour();
-    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
-}, 1000)
+        let currentTime = new Date();
+
+        hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+        min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+        sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+    }, 1000);
+});
+
